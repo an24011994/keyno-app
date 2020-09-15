7c2b08ed389bd7d763e104bef6c3b5d203f0f9dd
unknown <an24011994@gmail.com>
2020-09-15 21:07:12 +0700

    an

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
+import { SignUpComponent } from './view/sign-up/sign-up.component';
 import { SignInComponent } from './view/sign-in/sign-in.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  { path: '', component: SignInComponent }
+  { path: '', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
